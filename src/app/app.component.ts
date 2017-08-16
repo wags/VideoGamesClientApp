ec5d547cc038115aa52b6c42d70cd718a6353155
Matt Wagner <matt@wags.us>
2017-08-16 13:17:07 -0400

    Set up app component to call the API
    (This won't work due to CORS)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,12 @@ export class AppComponent {
     { title: 'Super Mario 64' },
     { title: 'Resident Evil' }
   ];
+
+  constructor(private http: Http) { }
+
+  loadVideoGames() {
+    this.http.get('http://localhost:5000/api/videogames')
+      .map((res: Response) => res.json())
+      .subscribe(data => console.log(data));
+  }
 }
