52373bff797b962479134465e36603c7685f0ad4
Matt Wagner <matt@wags.us>
2017-08-18 09:54:53 -0400

    Move video games list to its own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Video Games';
-  videoGames = [];
-
-  constructor(private http: Http) { }
-
-  ngOnInit() {
-    this.http.get('http://localhost:5000/api/videogames')
-      .map((res: Response) => res.json())
-      .subscribe(data => {
-        console.log(data);
-        this.videoGames = data;
-      });
-  }
-}
+export class AppComponent { }
--- src/app/video-games/video-games.component.ts
+++ src/app/video-games/video-games.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-video-games',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-games.component.css']
 })
 export class VideoGamesComponent implements OnInit {
+  title = 'Video Games';
+  videoGames = [];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.http.get('http://localhost:5000/api/videogames')
+      .map((res: Response) => res.json())
+      .subscribe(data => {
+        console.log(data);
+        this.videoGames = data;
+      });
   }
-
 }
