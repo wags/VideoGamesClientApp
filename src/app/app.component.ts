b17a88d83b660cf30e8906b39cf06e625269f028
Matt Wagner <matt@wags.us>
2017-08-18 08:47:14 -0400

    Update interface to load video games right away

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -7,13 +7,13 @@ import 'rxjs/add/operator/map';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works updated!';
+export class AppComponent implements OnInit {
+  title = 'Video Games';
   videoGames = [];
 
   constructor(private http: Http) { }
 
-  loadVideoGames() {
+  ngOnInit() {
     this.http.get('http://localhost:5000/api/videogames')
       .map((res: Response) => res.json())
       .subscribe(data => {
